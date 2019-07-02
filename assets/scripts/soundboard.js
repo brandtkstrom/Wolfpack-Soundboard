388cc8a23e7ffe985aad889e47005edf4d851e15
Brandt Strom <brandtkstrom@gmail.com>
2019-07-01 22:54:52 -0700

    add timothy section

--- assets/scripts/soundboard.js
+++ assets/scripts/soundboard.js
@@ -1,29 +1,3 @@
-const MAGICIAN = new Map([
-    ["not a magician", "not_a_magician.ogg"],
-    ["a cup", "a_cup.ogg"],
-    ["a whole cup", "whole_cup.ogg"],
-    ["scream", "magician_scream.ogg"],
-    ["broken", "broken.ogg"],
-    ["hold liquid", "hold_liquid.ogg"]
-]);
-const AXE_WATER = new Map([
-    ["i love him", "ld_love_him.ogg"],
-    ["doy boy", "fn_doy_boy.ogg"],
-    ["weninwaddu", "went_in_water.ogg"],
-    ["i dunno", "i_dunno.ogg"],
-    ["i can get it", "i_can_get_it.ogg"],
-    ["put me in", "dont_put_me_in.ogg"],
-    ["walk right in", "walk_right_in.ogg"],
-    ["done broke-ed", "done_broked_it.ogg"]
-]);
-const OTHER = new Map([["what is it", "what_is_it.ogg"]]);
-
-const SECTIONS = new Map([
-    ["Not a Magician", MAGICIAN],
-    ["Axe in the Water", AXE_WATER],
-    ["Other", OTHER]
-]);
-
 class Sound {
     constructor(file) {
         this.audio = new Audio(`assets/sounds/${file}`);
---
+++ assets/scripts/sounds.js
@@ -0,0 +1,30 @@
+const MAGICIAN = new Map([
+    ["not a magician", "not_a_magician.ogg"],
+    ["a cup", "a_cup.ogg"],
+    ["a whole cup", "whole_cup.ogg"],
+    ["scream", "magician_scream.ogg"],
+    ["broken", "broken.ogg"],
+    ["hold liquid", "hold_liquid.ogg"]
+]);
+
+const AXE_WATER = new Map([
+    ["i love him", "ld_love_him.ogg"],
+    ["doy boy", "fn_doy_boy.ogg"],
+    ["weninwaddu", "went_in_water.ogg"],
+    ["i dunno", "i_dunno.ogg"],
+    ["i can get it", "i_can_get_it.ogg"],
+    ["put me in", "dont_put_me_in.ogg"],
+    ["walk right in", "walk_right_in.ogg"],
+    ["done broke-ed", "done_broked_it.ogg"]
+]);
+
+const TIMOTHY = new Map([["it is me", "it_is_me.ogg"]]);
+
+const OTHER = new Map([["what is it", "what_is_it.ogg"]]);
+
+const SECTIONS = new Map([
+    ["Not a Magician", MAGICIAN],
+    ["Axe in the Water", AXE_WATER],
+    ["Timothy", TIMOTHY],
+    ["Other", OTHER]
+]);
