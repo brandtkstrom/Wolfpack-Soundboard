6bb840ee6428b70f519ec8b8039aa77b40990ef4
Brandt Strom <brandtkstrom@gmail.com>
2019-07-02 22:44:19 -0700

    add Hawk section

--- assets/scripts/sounds.js
+++ assets/scripts/sounds.js
@@ -9,7 +9,8 @@ const MAGICIAN = new Map([
 
 const AXE_WATER = new Map([
     ["i love him", "ld_love_him.mp3"],
-    ["doy boy", "fn_doy_boy.mp3"],
+    ["doy boy", "doy_boy.mp3"],
+    ["axe throw", "fn_doy_boy.mp3"],
     ["weninwaddu", "went_in_water.mp3"],
     ["i dunno", "i_dunno.mp3"],
     ["i can get it", "i_can_get_it.mp3"],
@@ -18,26 +19,31 @@ const AXE_WATER = new Map([
     ["done broke-ed", "done_broked_it.mp3"]
 ]);
 
-const TIMOTHY = new Map([
-    ["hey hey", "tim_hey_hey.mp3"],
-    ["it is me", "it_is_me.mp3"],
-    ["good", "tim_good.mp3"],
-    ["tasty", "tim_tasty.mp3"],
-    ["a 10", "tim_10.mp3"],
-    ["i really like it", "tim_good_it_is.mp3"]
-]);
-
-const OTHER = new Map([
-    ["what is it", "what_is_it.mp3"],
+const HAWK = new Map([
+    ["fuck", "hawk_fuck.mp3"],
+    ["pussy", "hawk_pussy.mp3"],
     ["close up", "jp_face.mp3"],
     ["fireball face", "fireball_jp.mp3"],
     ["fireball danger", "fireball_danger.mp3"],
     ["replay day", "replay_day.mp3"]
 ]);
 
+const TIMOTHY = new Map([
+    ["hey", "tim_hey.mp3"],
+    ["howya doin", "tim_howya_doin.mp3"],
+    ["it is me", "it_is_me.mp3"],
+    ["good", "tim_good.mp3"],
+    ["tasty", "tim_tasty.mp3"],
+    ["a 10", "tim_10.mp3"],
+    ["good it is", "tim_good_it_is.mp3"]
+]);
+
+const OTHER = new Map([["what is it", "what_is_it.mp3"]]);
+
 const SECTIONS = new Map([
     ["Not a Magician", MAGICIAN],
     ["Axe in the Water", AXE_WATER],
+    ["Hawk", HAWK],
     ["Timothy", TIMOTHY],
     ["Other", OTHER]
 ]);
